bd93c7e65c1b742ccacda9bcea04dd1324b947b6
IraMuomah <muomaho@gmail.com>
2023-05-20 09:18:06 -0700

    Updates

---
+++ precache-manifest.22e68b4b9eefbf53c714e2ee30e69dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5795afdac0c9aa750b92",
+    "url": "/Movie-Plus/static/css/main.427a3783.chunk.css"
+  },
+  {
+    "revision": "5795afdac0c9aa750b92",
+    "url": "/Movie-Plus/static/js/main.5795afda.chunk.js"
+  },
+  {
+    "revision": "51b078aa646a10602aea",
+    "url": "/Movie-Plus/static/js/1.51b078aa.chunk.js"
+  },
+  {
+    "revision": "a721bde0480d0b98797c",
+    "url": "/Movie-Plus/static/css/2.2026c5d5.chunk.css"
+  },
+  {
+    "revision": "a721bde0480d0b98797c",
+    "url": "/Movie-Plus/static/js/2.a721bde0.chunk.js"
+  },
+  {
+    "revision": "481ffeaa3db24ddcfea8",
+    "url": "/Movie-Plus/static/js/runtime~main.481ffeaa.js"
+  },
+  {
+    "revision": "85ed13546332a4043dec7d0a29c1c2ba",
+    "url": "/Movie-Plus/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/IraMuomah/Movie-Plus.git/precache-manifest.269156fcdeab1cfe4609ec10854c5682.js"
+  "/Movie-Plus/precache-manifest.22e68b4b9eefbf53c714e2ee30e69dbb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/IraMuomah/Movie-Plus.git/index.html", {
+workbox.routing.registerNavigationRoute("/Movie-Plus/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
