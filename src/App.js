162c2d62b1b9d086a62776baac126ff1fad1c614
IraMuomah <muomaho@gmail.com>
2023-05-21 09:55:22 -0700

    perfected HashRouter, renamed & styled some links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import './App.css';
 import React from 'react';
 import Header from './components/HeaderAndFooter/Header';
@@ -8,40 +7,50 @@ import Home from './pages/Home';
 import AllOffers from './pages/AllOffers';
 // import SingleOffer from './pages/SingleOffer';
 
-// import TopSlidder from './components/TopSlidder';
-// import RecomendedMovies from './components/RecomendedMovies';
-// import EndlessEntertainment from './components/EndlessEntertainment';
-// import LiveEvents from './components/LiveEvents';
+
 
 
 function App() {
   return (
     <>
-      <Router>
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/offers' element={<AllOffers/>}/>
           {/* <Route path='/offers/:ofrid' element={<SingleOffer/>}/> */}
-
-          {/* <Route exact path='/'>    
-            <TopSlidder />
-            <RecomendedMovies />
-            <EndlessEntertainment />
-            <LiveEvents />
-          </Route> */}
-          {/* initially the 'exact' key word was used when routes had switch cases, and if you didnt use them you could just put the home route('/') at the bottome of the routes to avoid errors, but nowadays both switch and bottom placements arent needed no more */}
         </Routes>
         <Footer />
-      </Router>
     </>
   );
 }
 
 export default App;
 
-// NOTE:
-// We are using HashRouter instead of normal BrowserRouter cause this is a Github project not a real project and...
-// GitHub Pages does not support browser history like your browser does. 
-// For example the route https://iramuomah.github.io/Movie-Plus/Offers doesn't help GitHub Pages understand where to point the user (since it is a frontend route).
-// To overcome this problem, we need to use a Hash Router instead of a Browser Router in our application. This type of router uses the hash portion of the URL to keep the UI in sync with the URL.
+
+// PERSONAL NOTE:
+/* We are using HashRouter instead of normal BrowserRouter cause this is a Github project not a real project and...
+GitHub Pages does not support browser history like your browser does. 
+For example the route https://iramuomah.github.io/Movie-Plus/Offers 
+doesn't help GitHub Pages understand where to point the user (since it is a frontend route).
+To overcome this problem, we need to use a Hash Router instead of a Browser Router in our application. 
+This type of router uses the hash portion of the URL to keep the UI in sync with the URL.
+*/
+
+// ALSO NOTE:
+// the messy code bellow can be used instead of using a neater <Home/> component/page
+
+/* import TopSlidder from './components/TopSlidder';
+import RecomendedMovies from './components/RecomendedMovies';
+import EndlessEntertainment from './components/EndlessEntertainment';
+import LiveEvents from './components/LiveEvents'; */
+          /* <Route exact path='/'>    
+            <TopSlidder />
+            <RecomendedMovies />
+            <EndlessEntertainment />
+            <LiveEvents />
+          </Route> */
+           
+/*initially the 'exact' key word was used when routes had switch cases, 
+and if you didnt use them you could just put the home route('/') at the bottome of the routes to avoid errors, 
+but nowadays both switch and bottom placements arent needed no more */
+
--- src/components/HeaderAndFooter/Header.js
+++ src/components/HeaderAndFooter/Header.js
@@ -5,43 +5,33 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
     <Navbar bg="dark" expand="lg" variant="dark">
       <Container >
-        <Navbar.Brand href="/">Movie Plus</Navbar.Brand>
+        {/* <Navbar.Brand href="/">Movie Plus</Navbar.Brand> */}
+        <Navbar.Brand>
+          <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>Movie Plus</Link>
+        </Navbar.Brand>         
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="offers">Offers</Nav.Link>
-            <NavDropdown title="Links" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Nav.Link><Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>Home</Link></Nav.Link>                   {/* <Nav.Link href="/">Home</Nav.Link> */}
+            <Nav.Link><Link to="/offers" style={{ color: 'inherit', textDecoration: 'inherit'}}> Offers</Link></Nav.Link>          {/* <Nav.Link href="offers">Offers</Nav.Link> */}
+            <NavDropdown title="Explore" id="navbarScrollingDropdown">
+              <NavDropdown.Item to="#">Stream Online</NavDropdown.Item>                   {/* Dont for get to add a <Link> tag when eventualy updating to="" key/attribute */}
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
+              <NavDropdown.Item to="#">Purchase Tickets</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item to="#">Buy BlueRay DVDs</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#" disabled>Movies</Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
             <Button variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
@@ -49,38 +39,45 @@ function Header() {
     </Navbar>
 
     <Navbar bg="secondary" className="pt-0 pb-0" variant="secondary">
-    <Container>
-    
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-between">
-        <Nav className="justify-content-start">
-        <Navbar.Text >
-        <a href="#login">Link A</a> &nbsp;
-        </Navbar.Text>
-        <Navbar.Text >
-        <a href="#login">Link B</a> &nbsp;
-        </Navbar.Text>
-        <Navbar.Text >
-        <a href="#login">Link C</a> &nbsp;
-        </Navbar.Text>
-        </Nav>
+      <Container>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-between">
+            <Nav className="justify-content-start">
+              <Navbar.Text >
+                <Link to="#" style={{color: 'inherit', textDecoration: 'line-through'}}>Stream</Link> &nbsp;
+              </Navbar.Text>
+              <Navbar.Text >
+                <Link to="#" style={{color: 'inherit', textDecoration: 'line-through'}}>Tickets</Link> &nbsp;
+              </Navbar.Text>
+              <Navbar.Text >
+                <Link to="/" style={{color: 'inherit', textDecoration: 'line-through'}}>BlueRay</Link> &nbsp;
+              </Navbar.Text>
+            </Nav>
 
-        <Nav className="justify-content-end">
-        <Navbar.Text >
-        <a href="#login">Link D</a> &nbsp;
-        </Navbar.Text>
-        <Navbar.Text >
-        <a href="/offers">Offers</a> &nbsp;
-        </Navbar.Text>
-        <Navbar.Text >
-        <a href="#login">Link F</a> &nbsp;
-        </Navbar.Text>
-        </Nav>
-    </Navbar.Collapse>
-    </Container>
+            <Nav className="justify-content-end">
+              <Navbar.Text >
+                <Link to="#" style={{color: 'inherit', textDecoration: 'line-through'}}>Contact Us</Link> &nbsp;
+              </Navbar.Text>
+              <Navbar.Text >
+                <Link to="/offers" style={{ color: 'none', textDecoration: 'inherit'}}> Offers</Link> &nbsp;
+              </Navbar.Text>
+              <Navbar.Text >
+                <Link to="/" style={{color: 'inherit', textDecoration: 'line-through'}}>About Us</Link> &nbsp;
+              </Navbar.Text>
+              {/* link sugestions: careers,  */}
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
 </>
   );
 }
 
 export default Header;
+
+
+// PERSONAL NOTE:
+/* 
+when using HashRouter you can use href="" Key/Atrribute, you need to instead use to="" , 
+which is best used in a react <Link> tag, using just <Nav.Link> or <a> only works with BrowserRouter
+ */
